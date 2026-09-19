1a76956e57fe55396568d6b3d8c63dbb983cb640
moshaban17 <136827227+moshaban17@users.noreply.github.com>
2026-09-19 16:34:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -318,11 +318,10 @@ async def menu_button(
     # SUBTITLE
     # -----------------------------------------------------
 
-        if data == "subtitle":
+            if data == "subtitle":
         context.user_data["mode"] = "subtitle"
 
-        settings = get_settings()
-
+        settings = load_settings()
         font_name = "تم رفع خط مخصص" if settings.get("font_message_id") else "Noto Sans Arabic"
         font_color = settings.get("font_color", "white")
         font_size = settings.get("font_size", "26")
